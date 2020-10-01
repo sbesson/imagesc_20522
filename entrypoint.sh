a5a414bdc27fa06fc21cdf2d2aac5aa422766e95
Sebastien Besson <seb.besson@gmail.com>
2020-10-01 17:06:15 +0100

    Add zlib and combine bf2raw/raw2ometiff

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 
 time /opt/bftools/bfconvert /tmp/test.fake /tmp/default.ome.tiff
 time /opt/bftools/bfconvert -tilex 512 -tiley 512 /tmp/test.fake /tmp/tiled.ome.tiff
-time /opt/bftools/bfconvert -compression lzw  -tilex 512 -tiley 512 /tmp/test.fake /tmp/tiled_compressed.ome.tiff
+time /opt/bftools/bfconvert -compression LZW  -tilex 512 -tiley 512 /tmp/test.fake /tmp/tiled_lzw.ome.tiff
+time /opt/bftools/bfconvert -compression zlib  -tilex 512 -tiley 512 /tmp/test.fake /tmp/tiled_zlib.ome.tiff
 
 mkdir /data/raw
-time /opt/bioformats2raw/bin/bioformats2raw /tmp/test.fake /tmp/raw/
-time /opt/raw2ometiff/bin/raw2ometiff /tmp/raw /tmp/bf2raw2ometiff.ome.tiff
+time /opt/bioformats2raw/bin/bioformats2raw /tmp/test.fake /tmp/raw/ && /opt/raw2ometiff/bin/raw2ometiff /tmp/raw /tmp/bf2raw2ometiff.ome.tiff
