4795b38670b146aac07a2ade9d92d44048250f9e
Sebastien Besson <seb.besson@gmail.com>
2020-10-01 16:58:30 +0100

    Add compression and time

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 
-/opt/bftools/bfconvert /tmp/test.fake /tmp/default.ome.tiff
-/opt/bftools/bfconvert -tilex -tiley /tmp/test.fake /tmp/tiled.ome.tiff
+time /opt/bftools/bfconvert /tmp/test.fake /tmp/default.ome.tiff
+time /opt/bftools/bfconvert -tilex 512 -tiley 512 /tmp/test.fake /tmp/tiled.ome.tiff
+time /opt/bftools/bfconvert -compression lzw  -tilex 512 -tiley 512 /tmp/test.fake /tmp/tiled_compressed.ome.tiff
 
 mkdir /data/raw
-/opt/bioformats2raw/bin/bioformats2raw /tmp/test.fake /tmp/raw/
-/opt/raw2ometiff/bin/raw2ometiff /tmp/raw /tmp/bf2raw2ometiff.ome.tiff
+time /opt/bioformats2raw/bin/bioformats2raw /tmp/test.fake /tmp/raw/
+time /opt/raw2ometiff/bin/raw2ometiff /tmp/raw /tmp/bf2raw2ometiff.ome.tiff
